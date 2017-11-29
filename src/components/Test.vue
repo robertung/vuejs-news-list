7a03b92609e82e4992f99ed6fa1b1a3925509690
Robert Ung <rothasar@gmail.com>
2017-11-29 07:53:36 -0800

    Addeding test files

---
+++ src/components/Test.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="test-view">
+    <br>
+    <label class="label" for="email">Password</label>
+    <br>
+    <input
+        v-validate="'required|truthy|punctuation|min:3'"
+        name="password"
+        type="password"
+        placeholder="Password"
+    >
+    <br>
+    <span
+        v-show="errors.has('password')"
+        class="error">
+          {{ errors.first('password') }}
+    </span>
+    <br>
+    {{ errors }}
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Test',
+    created () {
+      console.log(this);
+    }
+}
+</script>
